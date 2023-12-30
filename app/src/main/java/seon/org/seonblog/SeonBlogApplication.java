c6d3f253f265806635ed5b55d73ea9c691d815ff
Seonrilla <seonrilla97@gmail.com>
2023-12-30 23:00:27 +0900

    Create : Create NextJs And Spring Boot

---
+++ app/src/main/java/seon/org/seonblog/SeonBlogApplication.java
@@ -0,0 +1,13 @@
+package seon.org.seonblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeonBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeonBlogApplication.class, args);
+    }
+
+}
---
+++ app/src/test/java/seon/org/seonblog/SeonBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package seon.org.seonblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeonBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
